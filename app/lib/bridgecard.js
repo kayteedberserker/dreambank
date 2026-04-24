7d3bdf5db5f84717709242646c2a2f8f4583a73b
Umar Abdulkabir <ukabir902@gmail.com>
2026-04-24 08:20:29 +0100

    Initial commit from Create Next App

---
+++ app/lib/bridgecard.js
@@ -0,0 +1,34 @@
+
+const SECRET_KEY = process.env.BRIDGECARD_SECRET_KEY;
+const BASE_URL = process.env.BRIDGECARD_BASE_URL;
+
+export async function registerCardholder(firstName, lastName, email, phone, bvn) {
+    const response = await fetch(`${BASE_URL}/cardholder/register_cardholder_synchronously`, {
+        method: 'POST',
+        headers: {
+            'token': `Bearer ${SECRET_KEY}`,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            first_name: firstName,
+            last_name: lastName,
+            email: email,
+            phone: phone,
+            address: {
+                address: "123 Oreblogda Way",
+                city: "Osogbo",
+                state: "Osun",
+                country: "Nigeria",
+                postal_code: "100001",
+                house_no: "7"
+            },
+            identity: {
+                id_type: "NIGERIAN_BVN_VERIFICATION",
+                bvn: bvn, // Use "22222222222" for sandbox testing
+                selfie_image: "https://oreblogda.com/test-avatar.jpg"
+            }
+        }),
+    });
+
+    return await response.json();
+}
